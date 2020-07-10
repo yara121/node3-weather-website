ac70416ecf297d629bfda6ab237213fed890da21
yara <yaraeb121@gmail.com>
2020-07-10 17:30:11 +0200

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -89,6 +90,6 @@ app.get("*", (req, res) => {
     title: "Error",
   });
 });
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log("Server is up on port" + port);
 });
